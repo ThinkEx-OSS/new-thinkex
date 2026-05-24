721eb0c6ac712af22edd61f946097c48a04789ec
Urjit Chakraborty <135136842+urjitc@users.noreply.github.com>
2026-05-24 13:25:45 -0400

    Restore workspace resize handle styling

--- src/components/ui/resizable.tsx
+++ src/components/ui/resizable.tsx
@@ -10,7 +10,7 @@ function ResizablePanelGroup({
 		<ResizablePrimitive.Group
 			data-slot="resizable-panel-group"
 			className={cn(
-				"flex h-full w-full aria-[orientation=vertical]:flex-col",
+				"flex h-full w-full data-[panel-group-orientation=vertical]:flex-col",
 				className,
 			)}
 			{...props}
@@ -25,6 +25,7 @@ function ResizablePanel({ ...props }: ResizablePrimitive.PanelProps) {
 function ResizableHandle({
 	withHandle,
 	className,
+	children,
 	...props
 }: ResizablePrimitive.SeparatorProps & {
 	withHandle?: boolean;
@@ -33,14 +34,15 @@ function ResizableHandle({
 		<ResizablePrimitive.Separator
 			data-slot="resizable-handle"
 			className={cn(
-				"relative flex w-px items-center justify-center bg-border ring-offset-background after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:ring-1 focus-visible:ring-ring focus-visible:outline-hidden aria-[orientation=horizontal]:h-px aria-[orientation=horizontal]:w-full aria-[orientation=horizontal]:after:left-0 aria-[orientation=horizontal]:after:h-1 aria-[orientation=horizontal]:after:w-full aria-[orientation=horizontal]:after:translate-x-0 aria-[orientation=horizontal]:after:-translate-y-1/2 [&[aria-orientation=horizontal]>div]:rotate-90",
+				"relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 focus-visible:outline-none data-[panel-group-orientation=vertical]:h-px data-[panel-group-orientation=vertical]:w-full data-[panel-group-orientation=vertical]:after:left-0 data-[panel-group-orientation=vertical]:after:h-1 data-[panel-group-orientation=vertical]:after:w-full data-[panel-group-orientation=vertical]:after:translate-x-0 data-[panel-group-orientation=vertical]:after:-translate-y-1/2 [&[data-panel-group-orientation=vertical]>div]:rotate-90",
 				className,
 			)}
 			{...props}
 		>
-			{withHandle && (
-				<div className="z-10 flex h-6 w-1 shrink-0 rounded-lg bg-border" />
-			)}
+			{children ??
+				(withHandle ? (
+					<div className="z-10 flex h-6 w-1 shrink-0 rounded-lg bg-border" />
+				) : null)}
 		</ResizablePrimitive.Separator>
 	);
 }
