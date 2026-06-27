6d26860851e6933c670866995d8b4e7119af6598
Urjit Chakraborty <135136842+urjitc@users.noreply.github.com>
2026-06-27 17:26:05 -0400

    fix(message-scroller): update latest ref after render

--- src/components/ui/message-scroller-primitive/utils.ts
+++ src/components/ui/message-scroller-primitive/utils.ts
@@ -2,7 +2,11 @@ import * as React from "react";
 
 function useLatest<T>(value: T) {
 	const ref = React.useRef(value);
-	ref.current = value;
+
+	React.useLayoutEffect(() => {
+		ref.current = value;
+	}, [value]);
+
 	return ref;
 }
 
